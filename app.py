cc7ee1f36005339964b84c3747d2e954fdac20f3
Sanskar <sanskarxcix@gmail.com>
2021-03-16 19:25:43 +0530

    some work done

---
+++ app.py
@@ -0,0 +1,10 @@
+class Markov:
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+
+
+
+if __name__ == '__main__':
+    m = Markov()
+    
---
+++ get_book.py
@@ -0,0 +1,14 @@
+import requests
+
+def download(url, output: str = 'text'):
+    r = requests.get(url)
+    if r.status_code == 200:
+        with open(output, 'w') as file:
+            file.write(r.text)
+        print(f'{r.text[:100]}\n...\n{r.text[-100:]}\nwritten to file {output}')
+
+
+
+
+if __name__ == '__main__':
+    download('http://www.gutenberg.org/files/308/308-0.txt', '3menboat') 
